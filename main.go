d6d528a1508181d35a35a01af57749150d989664
yhidetoshi <hyajima.1014@gmail.com>
2019-06-30 07:59:57 +0900

    add switching org

--- main.go
+++ main.go
@@ -3,18 +3,47 @@ package main
 import (
 	"flag"
 	"github.com/mackerelio/mackerel-client-go"
-	"github.com/yhidetoshi/ore-mkr-cli/lib"
+	oremkrcli "github.com/yhidetoshi/ore-mkr-cli/lib"
 	"os"
 )
 
 var (
-	argList = flag.Bool("list", false, "host list")
-	mkrKey   = os.Getenv("MKRKEY")
-	client = mackerel.NewClient(mkrKey)
+	argOrg = flag.String("org", "", "set org")
+	argType = flag.String("type", "", "set type")
+	argHostname = flag.String("", "", "input image name")
+
+	argRetire = flag.Bool("retire", false, "retire host")
+
+	// set mkr key each org
+	mkrKeyOrgA   = os.Getenv("MKRKEY_OrgA")
+	mkrKeyOrgB  = os.Getenv("MKRKEY_OrgB")
+
+	client = mackerel.NewClient("")
+
+
+	OrgA = "orgA"
+	OrgB = "orgB"
 )
 
 func main() {
 	flag.Parse()
-	oremkrcli.FetchHost(client)
+
+	// switch mkr apikey
+	switch *argOrg{
+	case OrgA:
+		client = mackerel.NewClient(mkrKeyOrgA)
+
+	case OrgB:
+		client = mackerel.NewClient(mkrKeyOrgB)
+	}
+
+	// Host Commands
+	if *argType == "host"{
+		if *argRetire {
+
+		}else{
+			oremkrcli.FetchHost(client)
+		}
+	}
 
 }
