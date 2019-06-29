39b6bfb09ca4598fc9a01328ad098f7981c1b645
yhidetoshi <hyajima.1014@gmail.com>
2019-06-29 13:00:47 +0900

    test

---
+++ lib/host.go
@@ -0,0 +1,18 @@
+package oremkrcli
+
+import "fmt"
+
+//func FetchHostInfo(mkr *client) {
+
+
+func FetchHostInfo() {
+/*
+	hostId, _ := client.FindHosts(
+		&mackerel.FindHostsParam{Statuses: []string{"working", "standby", "maintenance", "poweroff"}})
+	for _, v := range hostId {
+		fmt.Println(v.Name)
+	}
+*/
+	fmt.Println("aaa")
+}
+
