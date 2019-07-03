c7d52928790da14e4e4c512144d7e5441b0f9f02
yhidetoshi <hyajima.1014@gmail.com>
2019-07-04 07:55:29 +0900

    add monitor func

--- lib/monitor.go
+++ lib/monitor.go
@@ -1,1 +1,28 @@
 package oremkrcli
+
+import (
+	"fmt"
+	"github.com/mackerelio/mackerel-client-go"
+)
+func FetchMonitorIDs(client *mackerel.Client){
+	monitors, err  := client.FindMonitors()
+	if err != nil {
+		fmt.Println("fail get monitors")
+	}
+	//fmt.Println(&monitors)
+
+	for _, v := range monitors {
+		//fmt.Printf("%v\t%v\n"  ,v.MonitorName(), v.MonitorID())
+		fmt.Println(v.MonitorID())
+	}
+	//DescribeMonitorByID(client)
+}
+
+/*
+func DescribeMonitorByID(client *mackerel.Client){
+	res, _ := client.GetMonitor("3DWLj5WU92E")
+
+	fmt.Println(res)
+
+}
+*/
